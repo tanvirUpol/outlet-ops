8bef076ca9c9caedfe08c6a71ed83fc10a6cfff8
tanvirUpol <tanvirrahmanupol@gmail.com>
2023-12-17 13:30:39 +0600

    updated

--- app/api/catLevel/[id]/route.ts
+++ app/api/catLevel/[id]/route.ts
@@ -5,41 +5,6 @@ import benchStoresModel from "@/models/BenchStoresModel";
 import { NextResponse } from "next/server";
 
 
-// export async function GET(req:Request, {params}: { params: { id:string } }) {
-
-//     // console.log(params);
-
-//     const {id} = params
-//     await connectMongoDB();
-//     const keyStoreData = await keyStoreModel.find({outlet_code: id}).lean()
-//     const catLevelData = await catLevelModel.find({outlet_code: id}).lean();
-
-
-//     const updatedA = catLevelData.map(objA => {
-//         const matchingObjectInB = keyStoreData.find(objB => objB.outlet_code === objA.outlet_code);
-    
-//         if (matchingObjectInB) {
-//         // Create a new object with added properties
-//         return {
-//             ...objA,
-//             outlet_division: matchingObjectInB.outlet_division,
-//             outlet_zone: matchingObjectInB.outlet_zone,
-//             outlet_status: matchingObjectInB.outlet_status,
-//             outlet_format: matchingObjectInB.outlet_format
-//         };
-//         }
-
-//         // console.log(objA);
-    
-//         // If no match is found, return the original object
-//         return objA;
-//     });
-
-
-
-//     return NextResponse.json(updatedA)
-
-// }
 
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
